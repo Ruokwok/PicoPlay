eceb117530e9456868956d3a0133be92c5ab48e2
Ruokwok <i@ruok.cc>
2023-08-09 20:42:32 +0800

    小游戏注册

--- src/main/java/net/starelement/picoplay/PicoPlay.java
+++ src/main/java/net/starelement/picoplay/PicoPlay.java
@@ -4,6 +4,7 @@ import cn.nukkit.Server;
 import cn.nukkit.plugin.PluginBase;
 import net.starelement.picoplay.cmd.PicoPlayCommand;
 import net.starelement.picoplay.game.Game;
+import net.starelement.picoplay.i18n.L;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,12 +17,14 @@ public class PicoPlay extends PluginBase {
         return pp;
     }
 
-    private List<Game> games = new ArrayList<>();
+    private List<Class> games = new ArrayList<>();
     private Profile profile = new Profile();
 
     @Override
     public void onLoad() {
         pp = this;
+        L.load();
+        L.setDefaultCode("chs");
         Server.getInstance().getCommandMap().register("PicoPlay", new PicoPlayCommand());
     }
 
@@ -30,8 +33,14 @@ public class PicoPlay extends PluginBase {
 
     }
 
-    public void registerGame(Game game) {
-
+    public void registerGame(Class game) {
+        if (Game.class.isAssignableFrom(game)) {
+            if (games.contains(game)) return;
+            games.add(game);
+            getLogger().info(L.get("chs", "pico.console.register.game") + game.getName());
+        } else {
+            throw new RuntimeException();
+        }
     }
 
     public Profile getProfile() {
